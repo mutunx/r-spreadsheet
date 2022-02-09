2261d09c004bafb56bb6953edab788114b8eb059
mutun <tun224813@gmail.com>
2022-02-09 22:59:29 +0800

    add css

--- src/components/adjustbar/adjustbar.js
+++ src/components/adjustbar/adjustbar.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Input from "./input/input";
+import {useStore} from "../store/store";
 
-function AdjustBar(props) {
+function AdjustBar() {
+    const store = useStore();
+
     return (
-        <div >
-            <label htmlFor={"columnCount"}>columnCount: </label>
-            <input id={"columnCount"}/>
-        </div>
+
+        <form className={"flex"}>
+            <Input value={store.tableInfo.rowCount} id={"rowCount"} label={"rowCount"}/>
+            <Input value={store.tableInfo.columnCount} id={"columnCount"} label={"columnCount"}/>
+            <Input value={store.tableInfo.cellHeight} id={"cellHeight"} label={"cellHeight"}/>
+            <Input value={store.tableInfo.cellWidth} id={"cellWidth"} label={"cellWidth"}/>
+            <Input value={store.tableInfo.rowHeaderWidth} id={"rowHeaderWidth"} label={"rowHeaderWidth"}/>
+            <Input value={store.tableInfo.rowHeaderHeight} id={"rowHeaderHeight"} label={"rowHeaderHeight"}/>
+            <Input value={store.tableInfo.columnHeaderWidth} id={"columnHeaderWidth"} label={"columnHeaderWidth"}/>
+            <Input value={store.tableInfo.columnHeaderHeight} id={"columnHeaderHeight"} label={"columnHeaderHeight"}/>
+            
+        </form>
     );
 }
 
